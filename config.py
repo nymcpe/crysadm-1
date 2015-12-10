e70cc2e9de698b29adb6901589837e1a87b962f9
nyko0311 <7289775@qq.com>
2015-12-11 05:20:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ __author__ = 'powergx'
 
 
 class RedisConfig():
-    def __init__(self, host, port, db, password=None):
+    def __init__(self, host, port, db, password):
         self.host = host
         self.port = port
         self.db = db
@@ -16,8 +16,8 @@ class Config(object):
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
     SESSION_TYPE = 'memcached'
     SECRET_KEY = '7e30485a-dd01-11e4-8abd-10ddb199c373'
-    REDIS_CONF = RedisConfig(host='10.10.71.37', port=51490, db=0，password="")
-    PASSWORD_PREFIX = ""
+    REDIS_CONF = RedisConfig(host='10.10.71.37', port=51490, db=0，password="oodFLc8oC4")
+    PASSWORD_PREFIX = "oodFLc8oC4oodFLc8oC4oodFLc8oC4"
     ENCRYPT_PWD_URL = None
     SERVER_IP = '0.0.0.0'
     SERVER_PORT = 80
